432c51bdc6df57eaab0ac8d01ca9b656a5e6bfb2
Vyacheslav <vyacheslavkozlovskih@gmail.com>
2022-07-20 19:39:25 +0500

    Добавлены роуты, которые взаимодействуют с маршрутизатором и перенаправляют на нужный контроллер и метод

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    // uri => controller/action
+    '/' => 'main/index',
+    
+    '/articles' => 'articles/index',
+    '/itineraries' => 'itineraries/index',
+    '/users' => 'users/index'
+);
